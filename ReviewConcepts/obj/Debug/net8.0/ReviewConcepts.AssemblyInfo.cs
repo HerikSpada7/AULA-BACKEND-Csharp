8040c0af49fb7d395bc264241eb3aedc5cbbc0d0
Herik Spada <gryzhn1@gmail.com>
2024-11-14 08:30:45 -0300

    Evoluindo no C-sharp e praticando desafios

--- ReviewConcepts/obj/Debug/net8.0/ReviewConcepts.AssemblyInfo.cs
+++ ReviewConcepts/obj/Debug/net8.0/ReviewConcepts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReviewConcepts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f1b2a4fd6333bba3f8d851af30715fef9d0d7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b365490061b645a643a19044a00339eaa0835ef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReviewConcepts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReviewConcepts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
